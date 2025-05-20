1ddd86c78e1bef90bfbe0c2055f758d529e12ca4
Antonio Kranjčina <tooony1998@hotmai.com>
2025-05-20 18:54:16 +0200

    Remove unused alias

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp) apply false
-//    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
 
 private val defaultJvmTarget = JvmTarget.JVM_17
